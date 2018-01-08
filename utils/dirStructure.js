34c0fd5909c57549cf0ff82d8afc33412f6281ad
bhwll <wang.liangliang@bohui.com.cn>
2018-01-08 09:58:02 +0800

    注释修改

--- utils/dirStructure.js
+++ utils/dirStructure.js
@@ -3,6 +3,12 @@ const path = require('path');
 const mime = require('mime');
 const config = require('../config/config');
 
+/**
+ * 入口
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ */
 const staticFn = (req, res) => {
     let url = decodeURI(req.url);
     // let fpath = path.join(__dirname + '/..'+url);
@@ -23,6 +29,13 @@ const staticFn = (req, res) => {
     })
 };
 
+/**
+ * 文件直接返回
+ * 
+ * @param {String} filePath 
+ * @param {*} req 
+ * @param {*} res 
+ */
 const fileFn = (filePath, req, res) => {
     fs.readFile(filePath,(err, data) => {
         if(err){
@@ -36,6 +49,13 @@ const fileFn = (filePath, req, res) => {
     })
 };
 
+/**
+ * 返回目录内容页面
+ * 
+ * @param {String} dirPath 
+ * @param {*} req 
+ * @param {*} res 
+ */
 const dirFn = (dirPath, req, res) => {
 	fs.readdir(dirPath, (err, files) => {
 		if(err){
@@ -49,6 +69,12 @@ const dirFn = (dirPath, req, res) => {
 	})
 };
 
+/**
+ * 生成页面模板
+ * 
+ * @param {Array} fileAndDirList 
+ * @param {String} url 
+ */
 const template = (fileAndDirList, url) => {
 	let fileAndDirListHtml = fileAndDirListHtmlFactory(fileAndDirList, url);
     return `<!DOCTYPE html>
@@ -64,6 +90,12 @@ const template = (fileAndDirList, url) => {
     `;
 };
 
+/**
+ * 生成目录内容html
+ * 
+ * @param {Array} fileAndDirList 
+ * @param {String} url 
+ */
 const fileAndDirListHtmlFactory = (fileAndDirList, url)=> {
 	fileAndDirList = fileAndDirList.sort((x, y) => {
 		if(!/\./.test(x)){
